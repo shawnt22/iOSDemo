e914b62e518c6379a18f195d97eacd1f1f54984b
tengsong <shawnt22@gmail.com>
2012-06-07 17:20:00 +0800

    创建GoogleAdMob工程

---
+++ GoogleAdMob/GoogleAdMob/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoogleAdMob
+//
+//  Created by 松 滕 on 12-6-7.
+//  Copyright (c) 2012年 shawnt22@gmail.com . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
