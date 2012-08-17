ad3bfe9dbc8c102cdb31b1e935aba77b8b3ec16d
tengsong <shawnt22@gmail.com>
2012-08-17 10:21:32 +0800

    添加Demo工程

---
+++ SSplitControllerDemo/SSplitControllerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SSplitControllerDemo
+//
+//  Created by 滕 松 on 12-8-17.
+//  Copyright (c) 2012年 滕 松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
