9e2f0252c0273d8087755208c21b351a1f2adcf3
tengsong <shawnt22@gmail.com>
2012-08-17 11:23:29 +0800

    更新结构文档

---
+++ SSplitControllerDemo/SSplitControllerDemo/SSplitController.h
@@ -0,0 +1,13 @@
+//
+//  SSplitController.h
+//  SSplitControllerDemo
+//
+//  Created by 滕 松 on 12-8-17.
+//  Copyright (c) 2012年 滕 松. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSplitController : NSObject
+
+@end
---
+++ SSplitControllerDemo/SSplitControllerDemo/SSplitController.m
@@ -0,0 +1,13 @@
+//
+//  SSplitController.m
+//  SSplitControllerDemo
+//
+//  Created by 滕 松 on 12-8-17.
+//  Copyright (c) 2012年 滕 松. All rights reserved.
+//
+
+#import "SSplitController.h"
+
+@implementation SSplitController
+
+@end
