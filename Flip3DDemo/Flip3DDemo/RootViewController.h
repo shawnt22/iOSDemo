3d36619f3a22a6feea216bd52d986267546f80e7
tengsong <shawnt22@gmail.com>
2012-09-27 17:49:18 +0800

    add rootviewcontroller

---
+++ Flip3DDemo/Flip3DDemo/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Flip3DDemo
+//
+//  Created by 滕 松 on 12-9-27.
+//  Copyright (c) 2012年 shawnt22@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ Flip3DDemo/Flip3DDemo/RootViewController.m
@@ -0,0 +1,38 @@
+//
+//  RootViewController.m
+//  Flip3DDemo
+//
+//  Created by 滕 松 on 12-9-27.
+//  Copyright (c) 2012年 shawnt22@gmail.com. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
