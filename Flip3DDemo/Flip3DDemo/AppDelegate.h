0a8c7617e4cbabbc1ab13b3bf5b9367ee0615098
tengsong <shawnt22@gmail.com>
2012-09-27 16:06:23 +0800

    创建工程

---
+++ Flip3DDemo/Flip3DDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flip3DDemo
+//
+//  Created by 滕 松 on 12-9-27.
+//  Copyright (c) 2012年 shawnt22@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Flip3DDemo/Flip3DDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Flip3DDemo
+//
+//  Created by 滕 松 on 12-9-27.
+//  Copyright (c) 2012年 shawnt22@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
