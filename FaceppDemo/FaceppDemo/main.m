0348de6dc234a106c9891ad91372a68a2fabb94d
ShawnTeng <shawnt22@gmail.com>
2014-04-01 12:07:11 +0800

    添加FaceppDemo

---
+++ FaceppDemo/FaceppDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FaceppDemo
+//
+//  Created by 佳佑 on 14-4-1.
+//  Copyright (c) 2014年 shawnt22@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FaceppDemo/FaceppDemoTests/FaceppDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  FaceppDemoTests.m
+//  FaceppDemoTests
+//
+//  Created by 佳佑 on 14-4-1.
+//  Copyright (c) 2014年 shawnt22@gmail.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FaceppDemoTests : XCTestCase
+
+@end
+
+@implementation FaceppDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
